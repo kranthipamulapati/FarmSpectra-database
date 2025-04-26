2d1b55713ac5035c4c6afc228533244a514d1719
Kranthi Pamulapati <pamulapati.kranthi@gmail.com>
2025-04-26 14:01:16 +0530

    calendar validation

--- pb_hooks/farm_calendar.pb.js
+++ pb_hooks/farm_calendar.pb.js
@@ -1,20 +1,22 @@
 onRecordCreateRequest((e) => {
-    const coordinates = JSON.parse(e.record.get("coordinates"));
+    const farm_fk = e.record.get("farm_fk");
+    const sowing_date = e.record.get("sowing_date");
+    const harvesting_date = e.record.get("harvesting_date");
 
     try {
         const res = $http.send({
             method: "POST",
             timeout: 120,
-            body: JSON.stringify({ coordinates }),
+            body: JSON.stringify({ farm_fk, sowing_date, harvesting_date }),
             headers: {
                 "content-type": "application/json",
             },
-            url: "http://172.25.160.1:3000/farms/new/validate",
+            url: "http://172.25.160.1:3000/farms/calendar/validate",
         });
 
         const jsonRes = res.json;
 
-        if (jsonRes.isPolygonValid === true) {
+        if (jsonRes.isCalendarValid === true) {
             e.next();
         } else {
             throw new BadRequestError(400, jsonRes.message);
@@ -22,4 +24,33 @@ onRecordCreateRequest((e) => {
     } catch (err) {
         throw new BadRequestError(400, err.message);
     }
-}, "farms");
+}, "farm_calendar");
+
+onRecordUpdateRequest((e) => {
+    const id = e.record.id;
+    const farm_fk = e.record.get("farm_fk");
+    const sowing_date = e.record.get("sowing_date");
+    const harvesting_date = e.record.get("harvesting_date");
+
+    try {
+        const res = $http.send({
+            method: "POST",
+            timeout: 120,
+            body: JSON.stringify({ id, farm_fk, sowing_date, harvesting_date }),
+            headers: {
+                "content-type": "application/json",
+            },
+            url: "http://172.25.160.1:3000/farms/calendar/validate",
+        });
+
+        const jsonRes = res.json;
+
+        if (jsonRes.isCalendarValid === true) {
+            e.next();
+        } else {
+            throw new BadRequestError(400, jsonRes.message);
+        }
+    } catch (err) {
+        throw new BadRequestError(400, err.message);
+    }
+}, "farm_calendar");
