5301704986ac26d1d54eb1f10ff99139e5d7cad8
Kranthi Pamulapati <pamulapati.kranthi@gmail.com>
2025-05-04 08:14:45 +0530

    move index data func

--- pb_hooks/farm_satellite_visit_data.pb.js
+++ pb_hooks/farm_satellite_visit_data.pb.js
@@ -15,3 +15,31 @@ onRecordAfterCreateSuccess((e) => {
 
     return e.next();
 }, "farm_satellite_visit_data");
+
+routerAdd(
+    "POST",
+    "/api/farms/satellite/index/data",
+    (e) => {
+        const data = new DynamicModel({
+            farm_fk: "",
+            index_fk: "",
+            satellite_fk: "",
+            visit_date: new Date(),
+        });
+
+        e.bindBody(data);
+
+        const res = $http.send({
+            method: "POST",
+            timeout: 120,
+            body: JSON.stringify(data),
+            headers: {
+                "content-type": "application/json",
+            },
+            url: "http://172.25.160.1:3000/farms/satellite/data/index",
+        });
+
+        e.json(200, res);
+    },
+    $apis.requireAuth()
+);
